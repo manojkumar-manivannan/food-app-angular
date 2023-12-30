f17520b8acfe84c4c36b0658c329737882bfd6b1
Manojkumar Manivannan <142196914+manojkumar-manivannan@users.noreply.github.com>
2023-12-30 11:32:44 -0800

    Update urls.ts
    Api url change

--- src/app/shared/constants/urls.ts
+++ src/app/shared/constants/urls.ts
@@ -1,6 +1,6 @@
 import { environment } from "src/environments/environment";
 
-const BASE_URL = environment.production? '' : 'http://manojkumardev.me:3000';
+const BASE_URL =  'http://manojkumardev.me:3000';
 
 export const FOODS_URL = BASE_URL + '/api/foods';
 export const FOODS_TAGS_URL = FOODS_URL + '/tags';
